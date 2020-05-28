fe5edc6c2e300b9b0b0b8b1abb4b9aed59eaed3f
Adam Madsen <madsen.adam@gmail.com>
2020-05-28 11:05:58 -0500

    I think we're gucci for multiple plays. Still can get rate limited downloading maps... probably need a queue.

--- createDatabase.js
+++ createDatabase.js
@@ -1,26 +1,19 @@
-var sqlite3 = require('sqlite3');
-var db = new sqlite3.Database('osuReports.db');
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database("osuReports.db");
+const fs = require("fs");
+const { promisify } = require("util");
 
-db.serialize(() => {
-  db.run(
-    'CREATE TABLE sessionsTable (sessionID INT, tweetID VARCHAR(30),date VARCHAR(30), osuUsername VARCHAR(100), sessionDuration VARCHAR(50), globalRank VARCHAR(15), difGlobalRank VARCHAR(15), countryRank VARCHAR(15), difCountryRank VARCHAR(15),' +
-      'level VARCHAR(10), difLevel VARCHAR(10), ' +
-      'accuracy VARCHAR(15), difAcc VARCHAR(15), totalPP VARCHAR(15), difPP VARCHAR(5), playCount VARCHAR(15), difPlayCount VARCHAR(15),' +
-      'countSSPlus VARCHAR(10), countSS VARCHAR(10), countSPlus VARCHAR(10), countS VARCHAR(10), countA VARCHAR(10))'
-  );
+const schema = fs
+  .readFileSync("schema.sql")
+  .toString("utf-8")
+  .split(/;\s*/)
+  .map((s) => s.trim())
+  .filter((s) => s !== "");
 
-  //db.run("INSERT INTO sessionsTable VALUES ('PenZa', '@PenZaIV', '10 Minutes', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22')");
-
-  db.run(
-    'CREATE TABLE playsTable (sessionID INT, background VARCHAR(100), title VARCHAR(100), version VARCHAR(100), artist VARCHAR(100), combo VARCHAR(50), bpm VARCHAR(3), playDuration VARCHAR(15), difficulty VARCHAR(6),' +
-      'playAccuracy VARCHAR(6), rank VARCHAR(3), mods VARCHAR(100), counts300 VARCHAR(5), counts100 VARCHAR(5), counts50 VARCHAR(5), countsMiss VARCHAR(5), playPP VARCHAR(6))'
-  );
-});
-
-// db.each("SELECT osuUsername, twitterUsername FROM playersTable", (err, row) => {
-//     console.log(row.osuUsername + " " + row.twitterUsername + " FROM DATABASE _-_-_-_-_-_-_")
-// });
-
-// db.each("SELECT globalRank, countryRank FROM sessionsTable", (err, row) => {
-//     console.log(row.globalRank + " " + row.countryRank + " FROM DATABASE _-_-_-_-_-_-_")
-// });
+for (const command of schema) {
+  db.serialize(() => {
+    promisify(db.run)
+      .bind(db)(command)
+      .catch((err) => console.log(err));
+  });
+}
