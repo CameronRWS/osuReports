6547caebfe3568492bcf2c7f4761ef5acc255302
Adam Madsen <adam@ajmadsen.com>
2020-07-19 10:59:52 -0500

    Add favicon to head

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,37 +9,19 @@
           <div v-if="player">
             <form action="/logout" method="POST">
               <nuxt-link to="/player" class="navbar-brand align-middle">
-                <strong> Signed in as: @{{ player.twitterUsername }} </strong>
+                <strong>Signed in as: @{{ player.twitterUsername }}</strong>
               </nuxt-link>
-              <button
-                type="submit"
-                class="btn btn-secondary my-2"
-                @click.prevent="logout"
-              >
-                Logout
-              </button>
+              <button type="submit" class="btn btn-secondary my-2" @click.prevent="logout">Logout</button>
             </form>
           </div>
-          <a v-else href="/twitter/login" class="btn btn-secondary my-2">
-            Login
-          </a>
+          <a v-else href="/twitter/login" class="btn btn-secondary my-2">Login</a>
         </div>
       </div>
     </header>
 
-    <section
-      v-if="flash && flash.length > 0"
-      class="jumbotron mt-0 mb-n1 py-2 rounded-0"
-    >
+    <section v-if="flash && flash.length > 0" class="jumbotron mt-0 mb-n1 py-2 rounded-0">
       <div class="container">
-        <div
-          class="alert alert-danger"
-          role="alert"
-          v-for="f in flash"
-          :key="f"
-        >
-          {{ f }}
-        </div>
+        <div class="alert alert-danger" role="alert" v-for="f in flash" :key="f">{{ f }}</div>
       </div>
     </section>
 
@@ -74,6 +56,11 @@ export default {
         .then(() => this.$store.dispatch("logout"))
         .then(() => this.$router.push("/"));
     }
+  },
+  head() {
+    return {
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    };
   }
 };
 </script>
