325abd51eed6af7a850765b4f5e97344db47757d
Cameron <CameronRWS@gmail.com>
2020-08-02 10:53:40 -0500

    added apple meta data

--- pages/report/_sessionId.vue
+++ pages/report/_sessionId.vue
@@ -9,12 +9,7 @@
       </a>
     </div>
     <div class="row justify-content-center">
-      <play
-        v-for="play in plays"
-        :key="play.date"
-        v-bind="play"
-        class="col-xl-6 col-12"
-      />
+      <play v-for="play in plays" :key="play.date" v-bind="play" class="col-xl-6 col-12" />
     </div>
   </section>
 </template>
@@ -36,7 +31,7 @@ export default {
       plays: await $api.getSessionPlays(params.sessionId),
       session: await $api.getSession(params.sessionId),
       sessionId: params.sessionId,
-      baseUrl: getBaseUrl(req)
+      baseUrl: getBaseUrl(req),
     };
   },
   /** @returns {boolean} */
@@ -48,36 +43,63 @@ export default {
     return {
       title: "osu! Report",
       meta: [
+        {
+          hid: "otherTitle",
+          property: "og:title",
+          // @ts-ignore
+          content: `${
+            this.session.osu.username
+          }'s osu! Report from ${dateFormat()}`,
+        },
+        {
+          hid: "otherContent",
+          name: "og:image",
+          // @ts-ignore
+          content: `${this.baseUrl}/api/player/sessions/${this.sessionId}/reportCard.png`,
+        },
         // hid is used as unique identifier. Do not use `vmid` for it as it will not work
         {
           hid: "twitterTitle",
           name: "twitter:title",
           // @ts-ignore
-          content: `osu! Report for ${this.session.osu.username}`
+          content: `${
+            this.session.osu.username
+          }'s osu! Report from ${dateFormat()}`,
         },
         {
           hid: "twitterDesc",
           name: "twitter:description",
           // @ts-ignore
-          content: `Click this link to see all ${this.plays.length} plays on the official osu! Reports website.`
+          content: `Click this link to see all ${this.plays.length} play(s) on the official osu! Reports website.`,
         },
         {
           hid: "twitterImage",
           name: "twitter:image",
           // @ts-ignore
-          content: `${this.baseUrl}/api/player/sessions/${this.sessionId}/reportCard.png`
+          content: `${this.baseUrl}/api/player/sessions/${this.sessionId}/reportCard.png`,
         },
         {
           hid: "twitterCard",
           name: "twitter:card",
-          content: "summary_large_image"
-        }
-      ]
+          content: "summary_large_image",
+        },
+      ],
     };
   },
   computed: {
-    ...mapState(["player"])
-  }
+    ...mapState(["player"]),
+    dateFormat() {
+      //@ts-ignore
+      let date = this.session.date;
+      //this is because some are in UTC some are epoch
+      if (date.toString().includes("Z")) {
+        date = new Date(date);
+      } else {
+        date = new Date(+date);
+      }
+      return date.toLocaleDateString("US-en");
+    },
+  },
 };
 </script>
 
