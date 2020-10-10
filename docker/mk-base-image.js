fe4e98f6cc1d0cb1d9e14dbd429e25c63dfd88d8
Adam Madsen <adam@ajmadsen.com>
2020-10-10 13:29:17 -0500

    Finish up CI Changes

--- docker/mk-base-image.js
+++ docker/mk-base-image.js
@@ -1,5 +1,28 @@
 const { promises: fsPromises } = require("fs");
-const path = require("path");
+const path = require("path").posix;
+const { spawn } = require("child_process");
+
+const REPLACE_PATTERN = new RegExp(`(# copy package.json)`);
+const DOCKERFILE_TEMPLATE = `\
+FROM node:lts-slim as builder
+WORKDIR /usr/src/app
+
+# copy package.json
+COPY yarn.lock package.json ./
+RUN yarn install --frozen-lockfile
+
+COPY tsconfig.json ./
+COPY ./packages ./packages
+RUN mv ./packages/common/src/consumerKeys.docker.ts ./packages/common/src/consumerKeys.ts
+RUN yarn workspaces run build
+
+FROM node:lts-slim
+WORKDIR /usr/src/app
+RUN chown node .
+
+COPY --from=builder --chown=node /usr/src/app/ ./
+USER node
+`;
 
 Array.prototype.any = function(predicate) {
   for (let i = 0; i < this.length; ++i) {
@@ -42,4 +65,38 @@ async function recursiveFind(base, fileName, options = {}) {
   );
 }
 
-module.exports = recursiveFind;
+recursiveFind("packages", "package.json", {
+  exclude: [/node_modules/]
+}).then(async paths => {
+  const copyLines = paths.map(p => `COPY ${p} ${p}`).join("\n");
+  const contents = DOCKERFILE_TEMPLATE.replace(
+    REPLACE_PATTERN,
+    `$1\n${copyLines}`
+  );
+  const [fileName] = [...process.argv.slice(2), "Dockerfile"];
+  await fsPromises.writeFile(fileName, contents);
+
+  if (process.env.GIT_INDEX_FILE) {
+    // we're running in a commit hook
+    const proc = spawn("git", ["add", fileName], {
+      env: process.env,
+      cwd: path.dirname(__dirname)
+    });
+    return new Promise((resolve, reject) => {
+      proc
+        .on("error", err => reject(err))
+        .on("exit", (code, signal) => {
+          if (signal !== null) reject(`signal ${signal}`);
+          if (code !== 0) reject(`code ${code}`);
+          resolve(code);
+        });
+    })
+      .catch(err => {
+        console.error(`Process exited abnormally: ${err}`);
+        process.exit(1);
+      })
+      .then(() => {
+        process.exit(0);
+      });
+  }
+});
--- packages/common/src/consumerKeys.docker.ts
+++ packages/common/src/consumerKeys.docker.ts
@@ -1,7 +1,7 @@
 export = {
-  osuApi_key: process.env.OSUAPI_KEY,
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  osuApi_key: process.env.OSUAPI_KEY!,
+  consumer_key: process.env.CONSUMER_KEY!,
+  consumer_secret: process.env.CONSUMER_SECRET!,
+  access_token: process.env.ACCESS_TOKEN!,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET!
 };
